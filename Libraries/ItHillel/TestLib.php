b9422c94cada8307b13d587908dab1bde06f1b45
apple <apple@MacBook-Pro-apple.local>
2024-01-29 21:27:21 +0200

    lesson_3

---
+++ Libraries/ItHillel/TestLib.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Libraries\ItHillel;
+
+class TestLib
+{
+}
--- index.php
+++ index.php
@@ -1,8 +1,3 @@
 <?php
 
-function showSmth()
-{
-    phpinfo();
-}
-
-showSmth();
+require_once __DIR__ . '/vendor/autoload.php';
